1c1759ba1c2fe4fc099566eaefd9eb0e068b9de7
XiangyuMa <1056816866@qq.com>
2026-04-20 17:57:10 +0800

    Back to Code first commit

---
+++ src/main/java/com/example/demo/config/MyApplicationRunner.java
@@ -0,0 +1,16 @@
+package com.example.demo.config;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Order(2)
+@Component
+public class MyApplicationRunner implements ApplicationRunner {
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("SpringBoot 启动完成，执行逻辑");
+
+    }
+}
