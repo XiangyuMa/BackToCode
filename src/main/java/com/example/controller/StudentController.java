b3aadce557a93bbe42ec49c90fc7b1eb805b791f
maxiangyu <1056816866@qq.com>
2026-04-24 09:19:05 +0800

    更新学生信息

--- src/main/java/com/example/controller/StudentController.java
+++ src/main/java/com/example/controller/StudentController.java
@@ -5,6 +5,7 @@ import com.example.service.StudentService;
 import com.example.util.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,11 @@ public class StudentController {
     public Result getAllStudent(){
         return  Result.success(studentService.getAllStudent());
     }
+
+    @GetMapping("/updateStatus")
+    public Result updateStatus(){
+        return  Result.success(studentService.batchUpdate());
+    }
+
 
 }
--- src/main/java/com/example/service/StudentService.java
+++ src/main/java/com/example/service/StudentService.java
@@ -16,8 +16,20 @@ public class StudentService {
 
 
     public List<Student> getAllStudent() {
+        // 查询所有学生
         StudentExample studentExample = new StudentExample();
-        studentExample.createCriteria().andAgeBetween((byte) 15, (byte) 30).andMajorEqualTo("软件工程");
+//        studentExample.createCriteria().andAgeBetween((byte) 15, (byte) 30).andMajorEqualTo("软件工程");
+        studentExample.createCriteria();
         return studentMapper.selectByExample(studentExample);
+    }
+
+    public int batchUpdate(){
+        // 批量更新：把所有年龄小于20岁的用户状态设为3
+        StudentExample example = new StudentExample();
+        example.createCriteria().andAgeLessThan((byte) 20);
+        Student updateStudent = new Student();
+        updateStudent.setStatus((byte)3);
+        studentMapper.updateByExampleSelective(updateStudent, example);
+        return 0;
     }
 }
