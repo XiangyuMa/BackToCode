a7436afbbf723be9b6fdd16ea8415fa96f2de983
XiangyuMa <1056816866@qq.com>
2026-05-06 10:56:14 +0800

    执行顺序： Bean 实例化 → @PostConstruct → InitializingBean.afterPropertiesSet() → 所有 ApplicationRunner/CommandLineRunner → ApplicationReadyEvent 事件

--- src/main/java/com/example/byteDance/Solution3.java
+++ src/main/java/com/example/byteDance/Solution3.java
@@ -4,7 +4,7 @@ import java.util.Stack;
 
 public class Solution3 {
     public static void main(String[] args) {
-        String s = "({}[])";
+        String s = "({}[]){}";
         System.out.println(isMatch(s));
     }
 
